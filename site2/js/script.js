07887139dab437efc691402ffdc057db9f21d2f6
BuildTools <unconfigured@null.spigotmc.org>
2021-04-20 22:44:03 -0400

    Portfolio v8

---
+++ site2/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", 
+    function(event) {
+        var version = "Site Version 0.8.0 (Beta)";
+        document.getElementById("email").innerText = version;
+        var update = "Last Updated: April 2021";
+        document.getElementById("update").innerText = update;
+    }
+);
