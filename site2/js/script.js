ed8d668c78bfc52653f4254acfb48b30beb4e070
BuildTools <unconfigured@null.spigotmc.org>
2021-04-20 22:47:56 -0400

    Portfolio v8.1

--- site2/js/script.js
+++ site2/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", 
     function(event) {
-        var version = "Site Version 0.8.0 (Beta)";
+        var version = "Site Version 0.8.1 (Beta)";
         document.getElementById("email").innerText = version;
         var update = "Last Updated: April 2021";
         document.getElementById("update").innerText = update;
