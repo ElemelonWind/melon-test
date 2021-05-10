e39ec7717d330c2062d3858ce74d33174bbd6261
BuildTools <unconfigured@null.spigotmc.org>
2021-05-10 16:34:39 -0400

    Portfolio v8.2

--- site2/js/script.js
+++ site2/js/script.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", 
     function(event) {
-        var version = "Site Version 0.8.1 (Beta)";
-        document.getElementById("email").innerText = version;
-        var update = "Last Updated: April 2021";
-        document.getElementById("update").innerText = update;
+        console.log("hi");
+        $ajaxUtils.sendGetRequest("/files/versions.json",
+                function(res) {
+                    var version = "Site Version " + res.version;
+                    var update = "Last Updated: " + res.lastUpdated;
+                    console.log(version + update);
+                    document.getElementById("email").innerText = version;
+                    document.getElementById("update").innerText = update;
+                }); 
+        
+        // var version = "Site Version 0.8.1 (Beta)";
+        // document.getElementById("email").innerText = version;
+        // var update = "Last Updated: April 2021";
+        // document.getElementById("update").innerText = update;
     }
 );
