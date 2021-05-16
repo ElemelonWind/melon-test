aa201d8c89d8add0451601a39f297e0d74d17dda
BuildTools <unconfigured@null.spigotmc.org>
2021-05-16 16:35:52 -0400

    Portfolio v9

--- site2/js/script.js
+++ site2/js/script.js
@@ -1,6 +1,31 @@
-document.addEventListener("DOMContentLoaded", 
+(function(global) {
+    var dc = {};
+    var doc = window.location.pathname.split("/").pop();
+    doc = doc.substring(0, doc.indexOf("."));
+    var homeHtml = "snippets/" + doc + "-snippet.html";
+
+    var insertHtml = function(selector, html) {
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHTML = html;
+    };
+
+    var showLoading = function(selector) {
+        var html = "<div class='text-center'>";
+        html += "<img src='images/ajax-loader.gif'></div>";
+        insertHtml(selector, html);
+    };
+
+    document.addEventListener("DOMContentLoaded", 
     function(event) {
-        console.log("hi");
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(homeHtml,
+            function(responseText) {
+                document.querySelector("#main-content").innerHTML = responseText;
+            }, false);
+    });
+
+    global.$dc = dc;
+
         $ajaxUtils.sendGetRequest("/files/versions.json",
                 function(res) {
                     var version = "Site Version " + res.version;
@@ -14,5 +39,21 @@ document.addEventListener("DOMContentLoaded",
         // document.getElementById("email").innerText = version;
         // var update = "Last Updated: April 2021";
         // document.getElementById("update").innerText = update;
-    }
-);
+
+        // document.addEventListener("scroll", 
+        //     function(event) {
+        //         var x = document.querySelector("#dropdownMenuButton1");
+        //         x.blur();
+        //         //document.querySelector(".dropdown-menu").collapse('hide');
+        //     }
+        // );
+}(window));
+
+// $(function() { // same as document.addEventListener("DOMContentLoaded"...
+//     // same as document.querySelector(".dropdown-menu").addEventListener("scroll")
+//     $("#dropdownMenuButton1").blur(function(event) {
+//         $(".dropdown-menu").collapse('hide');
+//     });
+
+// })
+// ^ that would've worked if the css didn't get messed up for some reason :/
